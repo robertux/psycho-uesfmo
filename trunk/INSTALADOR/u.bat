82232ffe1d451e785bae768ba0f5dcc6b7698c51
Ramayac <Ramayac@64633fbc-d753-0410-a4c9-9dae8bc8e378>
2009-03-19 16:42:59 +0000

    se versiona el PODEROSO script de instalacion con NSIS

---
+++ trunk/INSTALADOR/u.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Creado por Rodrigo Amaya Centeno, 19 Febrero 2006
+REM Suponiendo una instalacion fresca de MySQL (sin contraseña)
+"%PROGRAMFILES%\MySQL\MySQL Server 5.0\bin\"mysql -u root < "%TEMP%"\u.sql
---
+++ trunk/INSTALADOR/u.sql
@@ -0,0 +1,3 @@
+USE psicologia;
+GRANT ALL PRIVILEGES ON psicologia.* TO psychouser@localhost IDENTIFIED BY 'psychopwd' WITH GRANT OPTION;
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');
