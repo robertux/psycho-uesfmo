55fccdd4776f786d43af1080b3c8744d1a03c854
Ramayac <Ramayac@64633fbc-d753-0410-a4c9-9dae8bc8e378>
2009-03-21 23:27:19 +0000

    extra

--- trunk/WinApp/frmMain.Designer.cs
+++ trunk/WinApp/frmMain.Designer.cs
@@ -94,7 +94,7 @@ namespace WinApp
 			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.groupBox1.Location = new System.Drawing.Point(0, 0);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(907, 678);
+			this.groupBox1.Size = new System.Drawing.Size(804, 585);
 			this.groupBox1.TabIndex = 0;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Psycho Evaluador";
@@ -406,11 +406,13 @@ namespace WinApp
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.ClientSize = new System.Drawing.Size(907, 678);
+			this.ClientSize = new System.Drawing.Size(804, 585);
 			this.Controls.Add(this.groupBox1);
 			this.DoubleBuffered = true;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
 			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Psycho Evaluador";
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
